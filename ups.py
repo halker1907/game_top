0e9e2f7d24cd31b4b2eb4a7ebd7d168e6f8501f6
halker1907 <113712157+halker1907@users.noreply.github.com>
2022-11-22 19:40:54 +0300

    Add files via upload

--- ups.py
+++ ups.py
@@ -3,10 +3,12 @@ from random import choice,randint
 first_name = ("жиран","дрын","брысь","жлык","крикун")
 last_name = ("грозный","громкий","усатый","жирный","глупый")
 
+
 def make_hero(
 	name=None,
 	hp_now=None,
-	xp_now=0,
+	hp_max=None,
+	xp_now=10,
 	xp_next=1000,
 	attak=1,
 	defence=0,
@@ -37,6 +39,7 @@ def make_hero(
 		inventory = []
 	if not hp_now:
 		hp_now = randint(1,10)
+	if not hp_max:
 		hp_max = hp_now
 	if money is None:
 		money = randint(0,100)
@@ -44,13 +47,15 @@ def make_hero(
 	return [
 		name,
 		hp_now,
+		hp_max,
 		xp_now,
 		xp_next,
 		attak,
 		defence,
 		money,
 		level,
-		luck
+		luck,
+		inventory
 	]
 
 
@@ -68,29 +73,67 @@ def show_hero(hero):
 
 
 def levelup(hero: list):
-	if hero[4] >= hero[5]:
+	if hero[3] >= hero[4]:
 		hero[3] += 1
-		hero[5] = hero[3] * 100
+		hero[4] = hero[3] * 100
 		print(f"{hero[0]} получил {hero[3]} уровень\n")
 
 
-def buy(hero, price):
-	"""
-	[9] money - деньги
-	[10] inventory - cписок предметов
-"""
+def buy(hero, price,item):
 	if hero[7] >= price:
 		hero[7] -= price
-		hero[10].append("зелья")
-		print(f"{hero[0]} купил зелье за {price} менет")
+		hero[10].append({item})
+		print(f"{hero[0]} купил {item} за {price} менет")
 	else:
 		print(f"у {hero[0]} нет столько монет")
 
-def consume_item(hero: list, item: str):
+
+def consume_item(hero: list, index: str):
+	if index <= len(hero[10]) - 1:
+		print(f"{hero[0]} употрибил {hero[10][index]}")
+		if hero[10][index] == "зелье":
+			hero[1] += 10
+			if hero[1] > hero[2]:
+				hero[1] = hero[2]
+		elif hero[10][index] == "яблоко":
+			pass
+		else:
+			print("никокого эфекта")
+		hero[10].pop(index)
+	else:
+		print("нет такого предмета")
 
 
+def play_dice(hero, bet):
+	if bet > 0:
+		if hero[7] >= bet:
+			hero_score = randint(2, 12)
+			casino_score = randint(2, 12)
+			print(f"{hero[0]} выбросил кубики и выпало {hero_score}")
+			print(f" трактирщик выбросил кубики и выпало {casino_score}")
+			if hero_score > casino_score:
+				hero[7] += bet
+				print(f"{hero[0]} выиграл {bet}")
+			elif hero_score < casino_score:
+				hero[7] -= bet
+				print(f"{hero[0]} проиграл {bet}")
+			else:
+				print("ничья ")
+		else:
+			print(f"у {hero[0]} нет столько денег")
+	else:
+		print("такая ставка невозможна")
 
-p1 = make_hero()
-p2 = make_hero()
-show_hero(p1)
-show_hero(p2)
+
+def fight(hero):
+	"""
+	Cделать врага 
+	Зависит ли враг от уровня героя
+	обмен ударами пока у всех есть жизни
+	формула атаки и защита
+	После боя забрать опыт деньги и предметы
+	Проверить лэвел ап
+	Можно ли выпить зелье в бою?
+
+	"""
+	pass
